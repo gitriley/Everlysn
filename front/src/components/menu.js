861cb8692593e17bc88fff375090981cab963399
Riley <20868615+gitriley@users.noreply.github.com>
2018-11-26 21:16:41 -0800

    added styles for selected theme in dropdown menu

--- front/src/components/menu.js
+++ front/src/components/menu.js
@@ -2,21 +2,47 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { setTheme } from '../actions'
 
-function mapStateToProps(state) {
+function mapStateToProps(store) {
     return {
-
+        theme: store.theme
     };
 }
 
+const selectThemeStyle = {
+    'font-weight': '700',
+    'text-decoration': 'underline'
+}
+
+
 class menu extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+    
+    getStyle = (themeValue) => {
+        if (this.props.theme === themeValue) {
+            return selectThemeStyle
+        }
+    }
     render() {
         return (
             <div className='menu'>
                 <div className='menu-item_header'>Theme:</div>
 
-                <div className='menu-subitem' onClick={() => this.props.dispatch(setTheme('light'))}>   Light</div>
-                <div className='menu-subitem' onClick={() => this.props.dispatch(setTheme('dark'))}>   Dark</div>
-                
+                <div 
+                    className='menu-subitem' 
+                    onClick={() => this.props.dispatch(setTheme('light'))}
+                    style={this.getStyle('light')}>   
+                    Light
+                </div>
+
+                <div 
+                    className='menu-subitem' 
+                    onClick={() => this.props.dispatch(setTheme('dark'))}
+                    style={this.getStyle('dark')}>    
+                    Dark
+                </div>
             </div>
         );
     }
